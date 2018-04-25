94eb21e844258c85c88e300cc8f5ae4db3a18884
superimpose <reese@superimpose.biz>
2018-04-25 13:46:20 -0400

    Initial commit

---
+++ Nativity/Assets/Scripts/BasicMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BasicMove : NetworkBehaviour {
+
+    private float speed = 0.5f;
+
+    public override void OnStartLocalPlayer()
+    {
+        Material mat = GetComponent<Renderer>().material;
+        mat.color = Color.blue;
+    }
+	
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * speed/10);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.forward * -speed/10);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.up * -speed*10);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up * speed*10);
+        }
+    }
+}
---
+++ Nativity/Assets/Scripts/DebugLocation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugLocation : MonoBehaviour {
+
+    private Text text;
+
+    private void Start()
+    {
+        text = GetComponentInChildren<Text>();
+    }
+    void Update () {
+        text.text = transform.position.ToString();
+	}
+}
